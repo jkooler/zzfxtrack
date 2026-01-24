27157cba583a6f8536d8c773c95c6d78de8a3f7f
Jarno Koole <jarno.koole@adaptek.fi>
2026-01-24 12:31:29 +0200

    Tinkering with demo song

--- songs/demo-song.js
+++ songs/demo-song.js
@@ -1,10 +1,14 @@
 import { stack, note, s, slow } from "@strudel/core";
 
-export const bpm = 122;
+export const bpm = 120;
 
 export const pattern = // Demo song for Strudel to ZzFXM
-// Audio processing effects will not carry on to exported .json file that ZzFXM plays.
-// Note that in this early alpha version, there could be other issues as well.
+
+// 1. Audio processing effects will not carry on to exported .json file that ZzFXM plays.
+// 2. Make sure to use the "Instruments" instead of the synthesis provided by Strudel.
+// 3. Any sound produced by Strudel will not carry on to the exported ZzFXM songs.
+
+// This is an early alpha version, so there could be other issues as well.
 
 stack(
   // Kick
@@ -12,12 +16,18 @@ stack(
 
   // Bass
   note("c2 a1(3,8) c2(2,8) g1(3,8)").slow(2).s("demo-bass"),
+
+  // Hi-hat
+  sound("demo-hh-closed(16,16), [- demo-hh-open]*4"),
+
+  // Cowbell
+  sound("- - - - - - - [demo-cowbell]*2").slow(2),
   
   // Snare
-  note("~ c2 ~ c2").s("demo-clap"),
+  note("~ c2 ~ c2").s("demo-snare"),
   
-  // Funky Lead
-  note("g4 [bb3 c4] f3 [f4 eb4]").s("demo-lead"),
+  // Synth stab
+  note("g4 [bb3 c4] f3 [f4 eb4]").s("demo-synth-stab"),
 
   // Test pad
   note("<[c4,f4,bb3]@2 [d4,g4,b4] [d4,f4,a4]>*2").s("demo-pad")
--- songs/diagnostic-minimal.js
+++ songs/diagnostic-minimal.js
@@ -3,6 +3,7 @@ import { note, s } from "@strudel/core";
 export const bpm = 140;
 
 export const pattern = // Minimal diagnostics
+
 // Go to the instruments and try to change the parameters of "test-minimal" instrument.
 // Listen carefully how each parameter affect the sound, to see if there are apparent bugs.
 // Then export this song and listen if the output matches 1:1 with the ZzFXM exported song.
