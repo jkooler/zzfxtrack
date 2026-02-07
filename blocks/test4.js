9f4b8797803906b0962211696d3f32ae3e4b3df0
Jarno Koole <jarno.koole@adaptek.fi>
2026-02-07 18:55:07 +0200

    Block saving UX

---
+++ blocks/test4.js
@@ -0,0 +1,94 @@
+// Block: Test4
+// Created in tracker
+
+export const name = "Test4";
+export const description = "Created in tracker";
+
+export const pattern = `note("b4 a#4 b4 a#4 f4 b4 a4 g#4 a4 g#4 d4 c4 c#4 a3 g3 f#3").s("test-sine")`;
+
+// Optional: Tracker state for re-editing
+export const trackerState = {
+  "version": 1,
+  "channels": 4,
+  "steps": 16,
+  "grid": [
+    [
+      "b4",
+      "a#4",
+      "b4",
+      "a#4",
+      "f4",
+      "b4",
+      "a4",
+      "g#4",
+      "a4",
+      "g#4",
+      "d4",
+      "c4",
+      "c#4",
+      "a3",
+      "g3",
+      "f#3"
+    ],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
+  ],
+  "channelInstruments": [
+    "test-sine",
+    "",
+    "",
+    ""
+  ]
+};
