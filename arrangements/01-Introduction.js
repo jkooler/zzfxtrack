35ad65a69ddb8aba3ff9ee79885d5f6224678ef1
Jarno Koole <jarno.koole@adaptek.fi>
2026-03-10 01:43:54 +0200

    Gitignore update

---
+++ arrangements/01-Introduction.js
@@ -0,0 +1,40 @@
+// Arrangement: 01-Introduction
+
+export const name = "01-Introduction";
+export const scope = "system";
+
+export const arrangementState = {
+  "version": 1,
+  "name": "01-Introduction",
+  "bpm": 120,
+  "rows": [
+    {
+      "repeats": 4,
+      "blocks": [
+        "chords.js",
+        "bass.js"
+      ],
+      "loop": false
+    },
+    {
+      "repeats": 4,
+      "blocks": [
+        "drums.js",
+        "chords.js",
+        "bass.js"
+      ],
+      "loop": false
+    },
+    {
+      "repeats": 8,
+      "blocks": [
+        "drums.js",
+        "chords.js",
+        "synths.js",
+        "bass.js",
+        "cowbell.js"
+      ],
+      "loop": false
+    }
+  ]
+};
---
+++ arrangements/02-Simple.js
@@ -0,0 +1,21 @@
+// Arrangement: 02-Simple-sys
+
+export const name = "02-Simple";
+export const scope = "system";
+
+export const arrangementState = {
+  "version": 1,
+  "name": "02-Simple",
+  "bpm": 120,
+  "rows": [
+    {
+      "repeats": 16,
+      "blocks": [
+        "simple-drums.js",
+        "simple-chords.js",
+        "simple-synths.js"
+      ],
+      "loop": false
+    }
+  ]
+};
