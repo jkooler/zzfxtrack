a723859e7e8abc9cc09958a77d8add1c65c84e5b
Jarno Koole <jarno.koole@adaptek.fi>
2026-03-21 17:00:58 +0200

    Cleaning.

--- songs/index.js
+++
@@ -1,7 +0,0 @@
-export const songs = {
-  'demo-song': () => import('./01-demo-song.js'),
-  'demo-monophonic': () => import('./02-test-monophonic.js'),
-  'blocks-and-arrangement': () => import('./03-test-blocks-and-arrangement.js'),
-  'diagnostic-waves': () => import('./diagnostic-waves.js'),
-  'diagnostic-minimal': () => import('./diagnostic-minimal.js'),
-};
